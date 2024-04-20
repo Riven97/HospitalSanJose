4ee11a798b1f7b8f66f006816ae3069e0aab28f8
Cristhian Julian Montoya Arenas <bmx.lol@outlook.es>
2024-04-19 20:24:46 -0500

    subiendo otra ves las ultimas cosas

--- src/HospitalSanJose/model/Farmacia.java
+++ src/HospitalSanJose/model/Farmacia.java
@@ -10,4 +10,22 @@ public class Farmacia {
 
     private Inventario inventario;
 
+    /**
+     * Agregar medicamento para el paciente
+     *
+     * @param medicamento
+     */
+    /**
+     *
+     * public void agregarMedicamento(Medicamento medicamento) {
+     *
+     * for (Enfermedad e : enfermedades) { if
+     * (e.equals(medicamento.getEnfermedadAlivia())) { System.out.println("El
+     * paciente ya está siendo tratado con el medicamento " +
+     * medicamento.getNombre() + "."); return; } }
+     *
+     * enfermedades.add(medicamento.getEnfermedadAlivia());
+     * System.out.println("Se ha agregado el medicamento " +
+     * medicamento.getNombre() + " a la lista de tratamientos del paciente."); }
+     */
 }
